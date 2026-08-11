e764aeecc7bb381a0b8576e9f0daf6ac94a44f46
Nada <nada.korshan@harri.com>
2026-08-11 17:54:12 +0300

    create constructors

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,21 @@
+import java.awt.Color;
+
 public class Car extends Vehicle {
     
     public int chairNum;
     public boolean isFurniter;
+
+    public Car(){
+
+    }
+
+    public Car(int length, int width, Color color, int chairNum, boolean isFurniter){
+        this.length = length;
+        this.width = width;
+        this.color = color;
+        this.chairNum = chairNum;
+        this.isFurniter = isFurniter;
+    }
 
     
 }
