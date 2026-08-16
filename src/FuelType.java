7d54c678a6564b2c9a75a02983a5f1b457c43f45
Nada <nada.korshan@harri.com>
2026-08-16 10:36:27 +0300

    enum method

--- src/FuelType.java
+++ src/FuelType.java
@@ -2,6 +2,10 @@ public enum FuelType {
 
     DIESEL,
     GASOLINE,
-    ELECTRIC
+    ELECTRIC;
 
+    public String toString() {
+        String name = name().toLowerCase();
+        return Character.toUpperCase(name.charAt(0)) + name.substring(1);
+    }
 }
